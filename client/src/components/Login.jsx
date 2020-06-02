9fbeaa9c38de8d9225bd1658a7c0a8edbf66a2db
yakomoga <olga.dayneko@gmail.com>
2020-06-02 15:42:26 +0200

    fixing already registered link

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,7 +4,7 @@ class Login extends Component {
 
   render() {
     return ( <form>
-        <h3>Sign In</h3>
+        <h3>Log In</h3>
 
         <div className="form-group">
             <label>Email address</label>
